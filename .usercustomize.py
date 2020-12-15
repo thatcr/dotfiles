b72a30175f8d1ce518355d7a75288fc5c8bbd9d8
Rob Thatcher <thatcr@gmail.com>
2020-12-15 10:46:02 +0000

    initial setup.

---
+++ .usercustomize.py
@@ -0,0 +1,16 @@
+import logging
+
+import rich
+import rich.pretty
+import rich.logging
+
+
+rich.pretty.install()
+__builtins__['print'] = rich.print
+
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[rich.logging.RichHandler(rich_tracebacks=True)]
+)
+
