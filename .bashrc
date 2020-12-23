ec7f8de78906f4555407f26b3447503b4505e2df
Rob Thatcher <thatcr@gmail.com>
2020-12-23 11:23:40 +0000

    more dotfiles

---
+++ .bashrc
@@ -0,0 +1,7 @@
+
+
+alias procexp='${ProgramData}/ProcessExplorer/procexp.exe'
+alias more=less
+
+# include env vars
+source ~/.env
--- .usercustomize.py
+++ .usercustomize.py
@@ -1,18 +1,25 @@
 import logging
 
+import env_config
+
 import rich
 import rich.pretty
 import rich.logging
 
-
 rich.pretty.install()
-__builtins__["print"] = rich.print
 
-
+# use rich's nice
 FORMAT = "%(message)s"
 logging.basicConfig(
-    level="NOTSET",
+    level="INFO",
     format=FORMAT,
     datefmt="[%X]",
     handlers=[rich.logging.RichHandler(rich_tracebacks=True)],
 )
+
+cfg = env_config.Config()
+cfg.apply_log_levels()
+
+# for k, v in sorted(os.environ.items()):
+
+# 1/0
