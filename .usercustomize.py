7bcf732e8736a8b72b85c825529a8caace7e9404
Rob Thatcher <thatcr@gmail.com>
2021-01-26 17:18:03 +0000

    more rich output, use stderr

--- .usercustomize.py
+++ .usercustomize.py
@@ -6,17 +6,23 @@ import rich
 import rich.pretty
 import rich.logging
 import rich.traceback
+from rich.console import Console
 
 rich.pretty.install()
-rich.traceback.install()
+rich.traceback.install(show_locals=True)
 
 # use rich's nice
-FORMAT = "%(message)s"
+FORMAT = %(message)s"
 logging.basicConfig(
-    level="INFO",
+    level=logging.NOTSET,
     format=FORMAT,
     datefmt="[%X]",
-    handlers=[rich.logging.RichHandler(rich_tracebacks=True)],
+    handlers=[rich.logging.RichHandler(
+        level=logging.WARNING,
+        console=Console(stderr=True),
+        rich_tracebacks=True, 
+        tracebacks_show_locals=True
+    )],
 )
 
 cfg = env_config.Config()
