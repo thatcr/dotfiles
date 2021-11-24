7c45ca854501effdc42d9c2649bf57036beff04d
Rob Thatcher <5057953+thatcr@users.noreply.github.com>
2021-11-24 15:44:32 +0000

    init starship from bash

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rm -Rf ~/.oh-my-bash
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --force 
